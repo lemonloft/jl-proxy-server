bfe72a67e283e75cac17e25d8786fb0b56c7a2d2
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-12-10 12:12:03 -0800

    Make reviews module work

--- server/router.js
+++ server/router.js
@@ -12,6 +12,6 @@ app.use('/', express.static(path.join(__dirname, '../public')));
 app.use('/:hostId', express.static(path.join(__dirname, '../public')));
 app.use('http://localhost:3001/bundle.js', express.static(path.join(__dirname, '../reservation-module/public/bundle.js')));
 app.use('http://localhost:3003/bundle.js', express.static(path.join(__dirname, '../photo-gallery-module-gs/public/bundle.js')));
-app.use('http://localhost:3004/bundle.js', express.static(path.join(__dirname, '../reviews-module/public/index.js')));
+app.use('http://localhost:3004/index.js', express.static(path.join(__dirname, '../reviews-module/public/index.js')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
