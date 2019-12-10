1596c2df00629c3f2d534990b2be43bcb7e99626
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-12-10 10:56:22 -0800

    Make server working with static files

---
+++ server/router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+app.use('/:hostId', express.static(path.join(__dirname, '../public')));
+app.use('http://localhost:3001/bundle.js', express.static(path.join(__dirname, '../reservation-module/public/bundle.js')));
+app.use('http://localhost:3003/bundle.js', express.static(path.join(__dirname, '../photo-gallery-module-gs/public/bundle.js')));
+app.use('http://localhost:3004/bundle.js', express.static(path.join(__dirname, '../reviews-module/public/index.js')));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
