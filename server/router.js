0e2379688f8df9eaa4ffc6e885fca6ff8534fe8e
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-12-10 20:06:15 -0800

    Modify for s3 reserv bundle use
    Pointed router to appropriate aws s3 path. Removed reservation submodule

--- server/router.js
+++ server/router.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 app.use('/:hostId', express.static(path.join(__dirname, '../public')));
-app.use('http://localhost:3001/bundle.js', express.static(path.join(__dirname, '../reservation-module/public/bundle.js')));
+app.use('http://localhost:3001/bundle.js', express.static('https://lemonloftreservation.s3-us-west-1.amazonaws.com/bundle.js'));
 app.use('http://localhost:3003/bundle.js', express.static(path.join(__dirname, '../photo-gallery-module-gs/public/bundle.js')));
 app.use('http://localhost:3004/index.js', express.static(path.join(__dirname, '../reviews-module/public/index.js')));
 
